23fc86905a09f26b8f54d8645140bff9c7070d08
root <cbrandao2001@gmail.com>
2024-11-13 20:42:27 -0300

    code is running but the front is so ugly

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<?php include ('header.php'); ?>
-<head>
+    <head>
+    <?php include ('layouts/header.php'); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zodiac</title>
 </head>
 <body class="container-5">
     <h1 class="text-center">Descubra o seu signo</h1>
-    <form ide="signo-form" class="mt-4" action="show_zodiac_sign.php" method="post">
+    <form id="signo-form" class="mt-4" action="show_zodiac_sign.php" method="POST">
     <div class="form-group">
         <label for="data_nascimento">Data de nascimento: </label>
         <input type="date" id="data_nascimento" name="data_nascimento" class="form-control" required>
