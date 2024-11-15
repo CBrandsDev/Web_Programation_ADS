d056a9edb101d3f9f4e1701e64790ce3e3d1bb9f
Calil Brandão <cbrandao2001@gmail.com>
2024-11-15 12:13:21 -0300

    Some changes in index

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zodiac</title>
 </head>
-<body class="container-5">
-    <h1 class="text-center">Descubra o seu signo</h1>
-    <form id="signo-form" class="mt-4" action="show_zodiac_sign.php" method="POST">
-    <div class="form-group">
-        <label for="data_nascimento">Data de nascimento: </label>
-        <input type="date" id="data_nascimento" name="data_nascimento" class="form-control" required>
+<body class="container-fluid">
+    <div class="container">
+        <h1 class="text-center">Descubra seu Signo</h1>
+        <form id="signo-form" class="mt-4" action="show_zodiac_sign.php" method="POST">
+            <div class="form-group">
+                <label for="data_nascimento">Data de nascimento</label>
+                <input type="date" id="data_nascimento" name="data_nascimento" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Descobrir</button>
+        </form>
     </div>
-    <button type="submit" class="btn btn-primary">Descobrir</button>
-    </form>
 </body>
 </html>
